4ffdbf584e9536db182174f24f176501ec52e558
italobbarros <60980611+italobbarros@users.noreply.github.com>
2021-01-04 14:19:03 -0300

    Delete SendKeyboard.h

--- Software/Stream_Deck/src/SendKeyboard.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SENDKEYBOARD_H_
-#define	_SENDKEYBOARD_H_
-
-#include <Arduino.h>
-#include <BleKeyboard.h>
-
-void SendKeyboard (uint16_t AnalogReadButton1,uint16_t AnalogReadButton2);
-
-#endif /* _SENDKEYBOARD_H_ */
